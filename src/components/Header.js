75d216106614a1db5d7f6ed19494f9894915e1c7
kake <36560265+kake-t@users.noreply.github.com>
2021-07-01 21:40:09 +0900

    firebaseのログイン処理を追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import firebase from '../firebase'
+import '../App.css'
 
-const Header = () => {
+class Header extends Component {
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user })
+    })
+  }
+
+  login() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
+  logout() {
+    firebase.auth().signOut()
+  }
+
+  render() {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+      <div className="Login">
+        {this.state.user ? (
+          <button onClick={this.logout}>Google Logout</button>
+        ) : (
+          <button onClick={this.login}>Google Login</button>
+        )}
+      </div>
     )
+  }
 }
 
 export default Header
